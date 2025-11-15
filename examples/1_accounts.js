0514dbe0a3a945bc058b4e475950014471017e7d
Edward5051 <edante12345678@gmail.com>
2025-11-15 12:23:06 +0100

    added details to 1_accounts

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,10 +1,18 @@
 // Require packages
+require ("dotenv").config();
+const { ethers } = require("ethers");
 
 // Setup connection
+const URL = 'https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}';
+const provide = new ethers.JsonRpcProvider(URL);
 
+const ADDRESS = "0x6d8b201e121b8ae0421c03c21be0bb153dbf2560"
 async function main() {
   // Get balance
+ const balance = await provider.getBalance(ADDRESS)
+
   // Log balance
+  console.log(balance)
 }
 
 main()
